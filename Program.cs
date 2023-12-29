190c7c2c14067125712c19d5a34476630eee9636
Soffya <inguinha.fenandes@gmail.com>
2023-12-29 17:28:00 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("---------------- Smartphone Nokia ----------------");
+Smartphone nokia = new Nokia(numero: "(xx)xxxxx-xxxx", modelo: "Modelo EGM-150", imei:"789456123", memoria: 160);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagran");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("---------------- Smartphone Nokia ----------------");
+Smartphone iphone = new Iphone(numero: "(xx)xxxxx-xxxx", modelo: "Modelo KFW-130", imei:"123456789", memoria: 180);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
